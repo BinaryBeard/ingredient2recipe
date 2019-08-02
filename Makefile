e1177b83bf25c3cf4560b5943504cf9e7d4c61fa
BinaryBeard <sam.bantner@me.com>
2019-08-02 10:57:59 -0400

    Added some more testing

--- Makefile
+++ Makefile
@@ -1,11 +1,28 @@
 init:
 	pip3 install -r requirements.txt
 
+help:
+	@echo "Please use 'make <target>' where <target> is one of"
+	@echo "  clean     to remove build fiels"
+	@echo "  test      to run testing suite"
+	@echo "  coverage  to check test coverage"
+
 test:
 	python3 -m pytest -vv tests/
 
 coverage:
+	coverage run src/main.py fish chicken --dev --verbose
 	coverage run --source src -m py.test
+	coverage combine
 	coverage report --show-missing
+	coverage html
+
+clean:
+	-rm -rf .coverage
+	-rm -rf __pycache__
+	-rm -rf src/__pycache__
+	-rm -rf tests/__pycache__
+	-rm -rf .pytest_cache
+	-rm -rf htmlcov/
 
 .PHONY: init test coverage
--- tests/test_utility.py
+++ tests/test_utility.py
@@ -20,3 +20,22 @@ def test_get_json_url():
 def test_get_json_url_bad():
     test_json = utility.get_json_url('http://xkcd.com/abc/info.0.json')
     assert test_json['error'] == 'fatal'
+
+def test_handle_error_limit(capsys):
+    utility.handle_error('limit')
+    captured = capsys.readouterr()
+    assert captured.out == '\x1b[1m\x1b[40m\x1b[35mWell, dangit!\n\nIt looks like your API Key has reached its limit for the day...\n\nYou should probably fix that!\n'
+
+def test_handle_error_fatal(capsys):
+    utility.handle_error('fatal')
+    captured = capsys.readouterr()
+    print(repr(captured.out))
+    assert captured.out == '\x1b[1m\x1b[40m\x1b[35mI ran into some kind of fatal error...\n'
+
+def test_join_ingredients():
+    joined = utility.join_ingredients(['fish', 'chicken'])
+    assert joined == 'fish,chicken'
+
+def test_build_parser():
+    parser = utility.build_parser('abc123')
+    assert parser.description == 'Find a great recipe'
